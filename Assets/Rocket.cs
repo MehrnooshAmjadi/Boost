96db43a0469bff4c34671fd028247aac163a15f9
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2021-01-03 18:20:23 -0500

    Rotation is added to Rocket Ship

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -29,11 +29,11 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A)) 
         {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
