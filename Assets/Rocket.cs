6af33772a0a3cc5e77593725da3617b5f204787b
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2021-01-03 18:01:16 -0500

    update on Rocket.cs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,11 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
         if (Input.GetKey(KeyCode.A)) 
         {
             print("Rotating Left");
