3e817d75f1671ea5e7fd202b10e6571ff5ff73e8
Mehrnoosh Amjadi <M_AMJA@encs.concordia.ca>
2021-01-05 00:41:42 -0500

    Add Oscillator.cs

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 stringPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stringPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offcet = movementVector * movementFactor;
+        transform.position = stringPos + offcet;
+    }
+}
